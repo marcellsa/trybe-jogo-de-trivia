9948aeab47d50ba081e91d6823369681866d4d51
Rafael-Lacorte <rafaellacorte14@gmail.com>
2022-10-07 16:27:14 -0300

    implementa requisito 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import logo from './trivia.png';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="App">
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          {/* <Route path="/" component={  } /> */}
+          {/* <Route path="/formdisplay" component={ FormDataDisplay } /> */}
+        </Switch>
+      </main>
+    );
+  }
 }
+export default App;
