798600cf93bd81eba30fbb28867b61c368f79427
Rafael-Lacorte <rafaellacorte14@gmail.com>
2022-10-09 13:53:10 -0300

    implementa requisito 14

---
+++ src/pages/Feedback.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+class Feedback extends React.Component {
+  displayFeeback = (assertions) => {
+    if (assertions > 2) {
+      return 'Well Done!';
+    }
+    return 'Could be better...';
+  };
+
+  render() {
+    const { score, assertions } = this.props;
+    const feedbackMessage = this.displayFeeback(assertions);
+    return (
+      <div>
+        <h1 data-testid="feedback-text">{ feedbackMessage }</h1>
+        <h2 data-testid="feedback-total-score">
+          Pontução:
+          {' '}
+          { score }
+        </h2>
+        <h2 data-testid="feedback-total-question">
+          Numero de acertos:
+          {' '}
+          { assertions }
+        </h2>
+      </div>
+
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  assertions: state.player.assertions,
+  score: state.player.assertions,
+});
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Feedback);
