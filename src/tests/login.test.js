ec60d09605e2cac1db8d8c3c0cb345b56e1677cc
kayckhirt <kayckhirt15@gmail.com>
2022-10-10 18:42:55 -0300

    commit para o grupo poder visualizar

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -3,7 +3,7 @@ import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Provider } from 'react-redux';
 import { renderWithRouterAndRedux } from './helpers/renderWithRouterAndRedux';
-import { App } from '../App';
+import App from '../App';
 import Login from '../pages/Login';
 
 describe('Testa pagina de Login', () => {
@@ -12,12 +12,14 @@ describe('Testa pagina de Login', () => {
     const inputEmail = screen.getByPlaceholderText(/Email/i);
     const inputName = screen.getByPlaceholderText(/name/i);
     const btnLogin = screen.getByTestId('btn-play');
+    const btnSettings = screen.getByTestId('btn-settings');
     expect(inputEmail).toBeInTheDocument();
     expect(inputName).toBeInTheDocument();
     expect(btnLogin).toBeInTheDocument();
+    expect(btnSettings).toBeInTheDocument();
   })
-  test('Testa se é possível digitar nos inputs e habilitar o botão de "Play"', () => {
-  renderWithRouterAndRedux(<Login />);
+  test('Testa se é possível digitar nos inputs e habilitar o botão de "Play" e se ao clicar em "Play" muda para a url "/game"', () => {
+  const { history } = renderWithRouterAndRedux(<Login />);
   const emailTest = 'email@email.com';
   const name = 'nameTeste'
   const btnLogin = screen.getByTestId('btn-play');
@@ -28,5 +30,21 @@ describe('Testa pagina de Login', () => {
   userEvent.type(inputName, name);
   expect(btnLogin).toBeEnabled;
   userEvent.click(btnLogin);
+  const { location: { pathname } } = history;
+  setTimeout(() => {
+    expect(pathname).toBe('/game');
+  }, 3000);
   })
+  test('Testa se ao clicar em "Configurações", muda para a url "/settings"', () => {
+  const { history } = renderWithRouterAndRedux(<App />);
+  const btnSettings = screen.getByRole('button', {
+    name: /configurações/i,
+  });
+  userEvent.click(btnSettings);
+  const { location: { pathname } } = history;
+    expect(pathname).toBe('/settings');
+  })
+  test('testa o funcionamento do localStorage', () => {
+
+})
 })
